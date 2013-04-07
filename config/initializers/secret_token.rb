484975b1cc90d31094574b02072e7806405e2c58
didil <haritahadil@yahoo.fr>
2013-04-07 09:31:43 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreelanceDashboard::Application.config.secret_token = '21e93bce1b940c4ceb0b0354c513db2e053dfb8ed7562cb85f1185f1cf2313b387e5fbede55e1b73ba2628c4a7672b90fda6237989f8721e3aad5b0e3dca9ea2'
