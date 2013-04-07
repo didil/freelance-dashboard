f18842aedda3f0a94d98a919081a4523b0401437
didil <haritahadil@yahoo.fr>
2013-04-07 13:50:04 +0000

    add keyword feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 FreelanceDashboard::Application.routes.draw do
+  get "keywords_controller/create"
+
+  get "home/index"
+
+  get "job_feed/index"
+
   authenticated :user do
-    root :to => 'home#index'
+    root :to => 'job_feed#index'
   end
   root :to => "home#index"
   devise_for :users
   resources :users
+
+  resources :keywords
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130407093351) do
+ActiveRecord::Schema.define(:version => 20130407104833) do
+
+  create_table "keywords", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "roles", :force => true do |t|
     t.string   "name"
