b1c64b3462789eafc96431e633d56b3a7b888f6c
didil <haritahadil@yahoo.fr>
2013-04-12 10:39:01 +0000

    fixing no date_posted issue

--- app/helpers/job_feed_helper.rb
+++ app/helpers/job_feed_helper.rb
@@ -6,4 +6,9 @@ module JobFeedHelper
       0
     end
   end
+
+  def display_job_date_posted job
+    response ="Posted on : "
+    response += job.date_posted.strftime("%B, %-d %Y") unless job.date_posted.nil?
+  end
 end
