a911c720780fbf96e2343f4455471ded9180b698
didil <haritahadil@yahoo.fr>
2013-04-12 10:55:51 +0000

    find last job

--- app/helpers/job_feed_helper.rb
+++ app/helpers/job_feed_helper.rb
@@ -1,14 +1,14 @@
 module JobFeedHelper
   def first_job_id jobs
     if jobs and !jobs.empty?
-      jobs.first.id
+      jobs.map(&:id).max
     else
       0
     end
   end
 
   def display_job_date_posted job
-    response ="Posted on : "
+    response = "Posted on : "
     response += job.date_posted.strftime("%B, %-d %Y") unless job.date_posted.nil?
   end
 end
